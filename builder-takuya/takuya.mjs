5edcc23a4c11f4b053008ac7b3caee94104cedd2
rui-puipui <78154075+rui-puipui@users.noreply.github.com>
2023-06-01 22:29:50 +0900

    Update takuya.mjs

--- builder-takuya/takuya.mjs
+++ builder-takuya/takuya.mjs
@@ -10,6 +10,7 @@ function OutPut(HTML){
     //ファイル拡張子をHTMLに変換
     var newfileNM = filePath.split('.').slice(0, -1).join('.html');
 
+    var fs = WScript.CreateObject("Scripting.FileSystemObject");
     var file = fs.OpenTextFile(newfileNM, 2,true,-1);
 
     file.Write(HTML);

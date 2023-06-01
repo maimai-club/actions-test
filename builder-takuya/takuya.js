b3e61fcf4f9765aae64b5c8970a6a86929fd8741
rui-puipui <78154075+rui-puipui@users.noreply.github.com>
2023-06-01 22:07:50 +0900

    takuya add

---
+++ builder-takuya/takuya.js
@@ -0,0 +1,39 @@
+import { program } from "commander";
+import * as fs from "node:fs/promises";
+import { marked } from "marked";
+
+program.parse(process.argv);
+const filePath = program.args[0];
+
+//ファイルに出力
+function OutPut(HTML){
+    //ファイル拡張子をHTMLに変換
+    var newfileNM = new filePath.split('.').slice(0, -1).join('.html');
+
+    var file = fs.OpenTextFile(newfileNM, 2,true,-1);
+
+    file.Write(HTML);
+
+    //file.OutPut();
+
+    file.Close();
+}
+
+//MDをHTMLに変換
+function MDtoHTML(){
+    fs.readFile(filePath, { encoding: "utf8" }).then(file => {
+        // gfmオプションを無効にする
+        const html = marked.parse(file, {
+            gfm: false
+        });
+        return html;
+        //console.log(html);
+    
+    }).catch(err => {
+        console.error(err.message);
+        process.exit(1);
+    });
+}
+
+//DO
+OutPut(MDtoHTML());

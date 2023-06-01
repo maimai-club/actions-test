d67e9aac2c65b68a5c60828d79047b18925ffb14
rui-puipui <78154075+rui-puipui@users.noreply.github.com>
2023-06-01 22:48:55 +0900

    Update takuya.mjs

--- builder-takuya/takuya.mjs
+++ builder-takuya/takuya.mjs
@@ -1,5 +1,5 @@
 import { program } from "commander";
-import * as fs from "node:fs/promises";
+import * as fs from "fs";
 import { marked } from "marked";
 
 program.parse(process.argv);
@@ -11,7 +11,7 @@ function OutPut(HTML){
     var newfileNM = filePath.split('.').slice(0, -1).join('.html');
     
     // 書き込み
-    fs.writeFile(newfileNM, HTML[0], (err) => {
+    fs.writeFile(newfileNM, HTML, (err) => {
         if (err) throw err;
         console.log('正常に書き込みが完了しました');
     });
